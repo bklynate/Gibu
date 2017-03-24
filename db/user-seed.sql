e315b0b9df9ea686052975cae97ab7ab604dcc3f
Igor Kreymer <igorkr5@gmail.com>
2017-03-24 14:01:06 -0400

    working modal on homepage

---
+++ db/user-seed.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM donate_db.users
+LIMIT 0, 1000
+
+-- Date: 2017-03-24 01:35
+*/
+INSERT INTO `users` (`id`,`username`,`password`,`createdAt`,`updatedAt`) VALUES (1,'User1','$2a$10$paKmenI9wV.U4W1Yki03dOM4HaoF1Ac3WXgwVMkz1pT.4z/6..0uy','2017-03-24 00:57:44','2017-03-24 00:57:44');
+INSERT INTO `users` (`id`,`username`,`password`,`createdAt`,`updatedAt`) VALUES (2,'User2','$2a$10$dkqZq7P6DzlbHyMsTlbhguv3GyPTPRQWLYUPzajYzS1dx6KQLbvLu','2017-03-24 00:57:57','2017-03-24 00:57:57');
+INSERT INTO `users` (`id`,`username`,`password`,`createdAt`,`updatedAt`) VALUES (3,'User3','$2a$10$DaXlvlQYZ7NlVyoNqTXgfORW3Z6T3IEdMKBZD1ZsCrlw2Vh1ehU6u','2017-03-24 00:58:06','2017-03-24 00:58:06');
